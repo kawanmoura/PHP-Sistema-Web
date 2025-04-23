8bcf4c9af8e9ae435af0587c98a8b08d957fa3bb
Kawan Moura <kawanlemesmoura@gmail.com>
2025-04-23 14:23:32 -0300

    Sistema Web
    Adicionando primeiro commit.

---
+++ recebe-cadastro.php
@@ -0,0 +1,16 @@
+<?php
+    include("./conexao/conexao.php");
+
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $cidade = $_POST['cidade'];
+
+    $sql = "INSERT INTO listas (nome, email, cidade) VALUES ('$nome', '$email', '$cidade')";
+
+    if (mysqli_query($mysqli, $sql)) {
+        header("location: index.php");
+    } else {
+        echo "Error: " .$sql;
+    }
+?>
+
